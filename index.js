742397c6da610bcb29b133234b38799129aa99c9
Nixson Benitez <92173600+nixsonbenitez@users.noreply.github.com>
2026-02-02 11:56:07 -0600

    Here is the alert so users can interact with the code

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
-let f = 100;
 
-function convertToCelsius(f){ 
-  return (f - 32) * 5/9; 
+function convertToCelsius(f) { 
+  return (f - 32) * 5 / 9; 
 }
 
-function describeTempature(f){
+function describeTemperature(f) {
   const celsius = convertToCelsius(f);
 
   if (celsius < 0) {
-  return "very cold";
-}
-else if (celsius < 20){
-  return "cold";
-}
-else if (celsius < 30){
-  return "warm";
-}
-else if (celsius < 40){
-  return "hot";
-}
-else if (celsius >= 40){
-  return "very Hot";
-}
+    return "very cold";
+  } else if (celsius < 20) {
+    return "cold";
+  } else if (celsius < 30) {
+    return "warm";
+  } else if (celsius < 40) {
+    return "hot";
+  } else {
+    return "very hot";
+  }
 }
 
-console.log(describeTempature(f));
+const fahrenheitInput = prompt(
+  "Type in a temperature in Fahrenheit for it to be converted to Celsius:"
+);
+
+const fahrenheit = Number(fahrenheitInput);
+
+const celsius = convertToCelsius(fahrenheit);
+const description = describeTemperature(fahrenheit);
+
+alert(`That is ${celsius}C, which feels ${description}.`);
+
+
